1099f891de6ab253af74d81e85b7a3ab95c3f2ff
ruirodrigues <ruirodrigues95@outlook.pt>
2021-03-24 17:21:25 +0000

    changed navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,5 @@
 require('./bootstrap');
 
-// toggles the user menu dropdown when the navbar toggles is clicked
-document.querySelector('.navbar-toggler').addEventListener('click', () => {
-  $('#navUserMenu').toggleClass('show')
-})
-
 // stop propagation so the user menu dropdown doesn't disappear when it's clicked
 $(document).on('click', '.navbar #navUserMenu', function (e) {
   e.stopPropagation();
