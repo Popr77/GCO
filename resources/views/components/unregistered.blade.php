c1ae95e9e1f3b9c85d7c27edcc8c323af158c1cc
marcosdmf14 <marcos.ferreira1497@gmail.com>
2021-03-24 23:25:20 +0000

    Improved responsivity

--- resources/views/components/unregistered.blade.php
+++ resources/views/components/unregistered.blade.php
@@ -1,6 +1,8 @@
-<div class="container col-lg-12" style="background-color: whitesmoke; width:auto; height:400px; border: none">
+<!-- Initial Message -->
+
+<div class="container-fluid welcome-message">
     <div class="row" style="font-family: Poppins; display: flex; justify-content: center">
-        <div style="margin: 150px; text-align: center">
+        <div class="initial-message col-lg-6 col-md-8 col-sm-10">
             <h1>Learn today, teach tomorrow</h1>
             <h5 style="text-align: center; margin-top: 30px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate deleniti labore laudantium quae quia quod recusandae rem repellendus unde. Accusamus ea impedit incidunt iusto laboriosam omnis quia reprehenderit, sunt.</h5>
         </div>
@@ -9,21 +11,21 @@
 
 <!-- Recommended courses -->
 
-<div class="container col-lg-12" style="width:auto; height:400px; border: none">
+<div class="container-fluid recommended">
     <div class="row" style="font-family: Poppins; display: flex; justify-content: center">
-        <div style="text-align: center">
+        <div class="col-lg-6 col-md-8 col-sm-10" style="text-align: center">
             <h1 style="margin-top: 50px; margin-bottom: 20px">Recommended Courses</h1>
-            <h5 style="margin: 50px; text-align: center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate deleniti labore laudantium quae quia quod recusandae rem repellendus unde. Accusamus ea impedit incidunt iusto laboriosam omnis quia reprehenderit, sunt.</h5>
+            <h5 style="margin-top: 50px; text-align: center; ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate deleniti labore laudantium quae quia quod recusandae rem repellendus unde. Accusamus ea impedit incidunt iusto laboriosam omnis quia reprehenderit, sunt.</h5>
         </div>
     </div>
 </div>
 
 <!-- Cards -->
 
-<div style="display: flex; flex-direction: row; justify-content: center;">
+<div style="display: flex; flex-direction: row; justify-content: center; padding-bottom: 50px">
 
     <div class="card" style="width: 20rem; font-family: Poppins; align-items: center">
-    <img class="card-img-top" src="..." alt="Image course 1">
+    <img class="card-img" src="..." alt="Image course 1">
     <div class="card-body">
         <h5 class="card-title">Course 1</h5>
         <p class="card-text">Course 1 text</p>
@@ -54,11 +56,11 @@
 
 <!-- Join us -->
 
-<div class="container col-lg-12" style="background-color: whitesmoke; width:auto; height:400px; border: none">
+<div class="container-fluid col-lg-12" style="background-color: whitesmoke; border: none">
     <div class="row" style="font-family: Poppins; display: flex; justify-content: center">
         <div style="margin: 50px; text-align: center">
             <h1>Join Us</h1>
-            <h5 style="text-align: center; margin-top: 30px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate deleniti labore laudantium quae quia quod recusandae rem repellendus unde. Accusamus ea impedit incidunt iusto laboriosam omnis quia reprehenderit, sunt.</h5>
+            <h5 style="text-align: center; margin-top: 30px">Learning keeps you ahead. Acquire new skills that will impress everyone.</h5>
             <a href="#" class="btn btn-primary" style="margin-top: 50px">Get Started</a>
         </div>
     </div>
