c2f8538b9b2a63d3aa95d1ec1218f2bbe85c46b6
marcosdmf14 <marcos.ferreira1497@gmail.com>
2021-03-30 17:18:33 +0100

    finish registered and unregistered pages

--- resources/views/components/unregistered.blade.php
+++ resources/views/components/unregistered.blade.php
@@ -1,7 +1,7 @@
 <!-- Initial Message -->
 
 <div class="container-fluid welcome-message">
-    <div class="row" style="font-family: Poppins; display: flex; justify-content: center">
+    <div class="row" style="display: flex; justify-content: center">
         <div class="initial-message col-lg-6 col-md-8 col-sm-10">
             <h1>Learn today, teach tomorrow</h1>
             <h5 style="text-align: center; margin-top: 30px">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci cupiditate deleniti labore laudantium quae quia quod recusandae rem repellendus unde. Accusamus ea impedit incidunt iusto laboriosam omnis quia reprehenderit, sunt.</h5>
@@ -24,33 +24,33 @@
 
 <div style="display: flex; flex-direction: row; justify-content: center; padding-bottom: 50px">
 
-    <div class="card" style="width: 20rem; font-family: Poppins; align-items: center">
-    <img class="card-img" src="..." alt="Image course 1">
+    <div class="card" style="width: 20rem; align-items: center; margin-right: 20px;">
+    <img class="card-img-top" src="https://cdn.datafloq.com/cache/blog_pictures/878x531/artificial-intelligence-future-of-programming.jpg" alt="Image course 1">
     <div class="card-body">
-        <h5 class="card-title">Course 1</h5>
-        <p class="card-text">Course 1 text</p>
-        <a href="#" class="btn btn-primary">Go to course</a>
+        <h5 class="card-title text-center">Course</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores atque culpa cum cumque eligendi eveniet excepturi, expedita ipsa magni minima, minus, molestias nostrum odio officia perspiciatis praesentium quas rerum sit!</p>
     </div>
+        <a href="#" class="btn btn-primary d-flex justify-content-center" style="margin: 10px">See full course</a>
     </div>
 
-    <div class="card" style="width: 20rem; font-family: Poppins; align-items: center">
-    <img class="card-img-top" src="..." alt="Image course 2">
+    <div class="card" style="width: 20rem; align-items: center; margin-right: 20px">
+    <img class="card-img-top" src="https://cdn.datafloq.com/cache/blog_pictures/878x531/artificial-intelligence-future-of-programming.jpg" alt="Image course 2">
     <div class="card-body">
-        <h5 class="card-title">Course 2</h5>
-        <p class="card-text">Course 2 text</p>
-        <a href="#" class="btn btn-primary">Go to course 2</a>
+        <h5 class="card-title text-center">Course</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam beatae corporis debitis incidunt ipsa laboriosam molestiae quaerat reprehenderit sapiente vero! Aliquam commodi, consequuntur eum laborum maiores nulla perspiciatis reprehenderit sapiente!</p>
+
     </div>
+        <a href="#" class="btn btn-primary d-flex justify-content-center" style="margin: 10px">See full course</a>
     </div>
 
-    <div class="card" style="width: 20rem; font-family: Poppins; align-items: center">
-    <img class="card-img-top" src="..." alt="Image course 3">
+    <div class="card" style="width: 20rem; align-items: center">
+    <img class="card-img-top" src="https://cdn.datafloq.com/cache/blog_pictures/878x531/artificial-intelligence-future-of-programming.jpg" alt="Image course 3">
     <div class="card-body">
-        <h5 class="card-title">Course 3</h5>
-        <p class="card-text">Course 3 text</p>
-        <a href="#" class="btn btn-primary">Go to course 3</a>
+        <h5 class="card-title text-center">Course</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum error explicabo rem repellat sequi similique. Ab consequatur fuga modi officia velit! Amet nemo quibusdam veritatis? Beatae dolor mollitia nemo tenetur.</p>
     </div>
+        <a href="#" class="btn btn-primary d-flex justify-content-center" style="margin: 10px">See full course</a>
     </div>
-
 </div>
 
 
