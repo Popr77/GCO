e6155084f8c4029230a8cc35933cd3b6db369ead
ruirodrigues <ruirodrigues95@outlook.pt>
2021-04-30 17:39:41 +0100

    modified UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTypeSeeder::class);
-        \App\Models\User::factory(10)->create();
         $this->call(UserSeeder::class);
+        \App\Models\User::factory(10)->create();
         $this->call(CategorySeeder::class);
         $this->call(SubCategorySeeder::class);
         $this->call(SubSubCategorySeeder::class);
