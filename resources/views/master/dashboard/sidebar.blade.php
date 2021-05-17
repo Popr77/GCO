001d3e61e3931307e21d29fb505deb1be8419905
ruirodrigues <ruirodrigues95@outlook.pt>
2021-05-17 17:15:14 +0100

    fixed filter by category, changed link to go back to website, removed unused css, added stop propagation to dropdown menu

--- resources/views/master/dashboard/sidebar.blade.php
+++ resources/views/master/dashboard/sidebar.blade.php
@@ -1,7 +1,8 @@
 <div class="bg-light border-right" id="sidebar-wrapper">
     <div class="sidebar-heading">GCO</div>
-    <div class="list-group list-group-flush">
+    <div class="list-group list-group-flush" id="sidebar-menu">
         <a href="#" class="list-group-item list-group-item-action bg-light nav-link">Dashboard</a>
         <a href="{{ route('d-course-index') }}" class="list-group-item list-group-item-action bg-light nav-link">Courses</a>
+        <a href="{{ route('home') }}" class="list-group-item list-group-item-action bg-light nav-link" id="back-to-site-btn">Go Back to Website</a>
     </div>
 </div>
