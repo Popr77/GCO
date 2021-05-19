cdc6584c6eef59b0f9b70424bcdab80471c2bd25
popr77 <joaopaulo.vasconcelos@hotmail.com>
2021-05-19 19:44:04 +0100

    margin fix (lesson)

--- resources/views/components/lessons/lessons-list.blade.php
+++ resources/views/components/lessons/lessons-list.blade.php
@@ -9,7 +9,7 @@
 @endif
 <div class="col-12 text-center mb-5">
     <h1 class="mt-5">Lesson List</h1>
-    <div class="text-left mb-2 ml-5 mt-1">
+    <div class="text-left mb-2 px-5 ml-5 mt-1">
         <a class="btn btn-primary" href="{{ url('dashboard/courses')}}" role="button">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"></path>
