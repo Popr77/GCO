7d5fc335ec3fb66731fcd702c1aa0474a8425bcf
ruirodrigues <ruirodrigues95@outlook.pt>
2021-05-23 00:11:14 +0100

    fixed redirect to home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
     public function index()
     {
         if (auth()->check())
-            return view('pages.registered');
+            return redirect(route('home'));
 
         return view('pages.unregistered');
     }
