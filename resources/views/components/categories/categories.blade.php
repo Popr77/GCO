07c4ac19e08e454f442400b5e1e3e7a863ebed14
ruirodrigues <ruirodrigues95@outlook.pt>
2021-05-30 20:12:55 +0100

    modified 'go back' arrow so it goes to / instead of home

--- resources/views/components/categories/categories.blade.php
+++ resources/views/components/categories/categories.blade.php
@@ -10,7 +10,7 @@
 <h1 class="text-center mt-5">Categories</h1>
 
 <div class="d-flex justify-content-between mb-2 px-3 ml-5 mt-3">
-    <a class="btn btn-primary" href="{{url('/home')}}" role="button">
+    <a class="btn btn-primary" href="{{ url('/')}}" role="button">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-arrow-left-short" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"></path>
         </svg>
